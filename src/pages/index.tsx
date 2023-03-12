c5d756dfa6e88a0f5d3d54acbc860f82caa434b1
Matias Fuentes <matiasf.cortez@gmail.com>
2023-03-12 18:24:16 -0300

    :sparkles: (pages): Added homepage styles and functionalities

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-import Button from '@UI/Button';
+import Card from '@UI/Card';
 import Container from '@UI/Container';
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -15,14 +17,40 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="text-4xl font-bold">Home</h1>
-        <p className="text-xl">Bienvenido a Onesta farm</p>
-        <div className="flex gap-5">
-          <Button icon="+" type="primary">
-            Button
-          </Button>
-          <Button type="secondary">Button</Button>
-          <Button type="link">Button</Button>
+        <div className="flex flex-col items-center mb-5">
+          <Image
+            draggable={false}
+            width={300}
+            height={100}
+            src="/logo.png"
+            alt="logo"
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-5">
+          <Link href="/farmers">
+            <Card className="flex flex-col items-center hover:scale-105 transition-transform">
+              <Image width={100} height={100} src="/farmer.png" alt="cliente" />
+              <h2 className="text-xl mt-2">Agricultores</h2>
+            </Card>
+          </Link>
+          <Link href="/clients">
+            <Card className="flex flex-col items-center hover:scale-105 transition-transform">
+              <Image width={100} height={100} src="/client.png" alt="cliente" />
+              <h2 className="text-xl mt-2">Clientes</h2>
+            </Card>
+          </Link>
+          <Link href="/crops">
+            <Card className="flex flex-col items-center hover:scale-105 transition-transform">
+              <Image width={100} height={100} src="/crops.png" alt="cliente" />
+              <h2 className="text-xl mt-2">Cosechas</h2>
+            </Card>
+          </Link>
+          <Link href="/fruits">
+            <Card className="flex flex-col items-center hover:scale-105 transition-transform">
+              <Image width={100} height={100} src="/fruits.png" alt="cliente" />
+              <h2 className="text-xl mt-2">Frutas</h2>
+            </Card>
+          </Link>
         </div>
       </main>
     </Container>
