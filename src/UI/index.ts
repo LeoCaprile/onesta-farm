8fa08c3dc80edb62af7c6a4e98e8e9c9691de15d
Matias Fuentes <matiasf.cortez@gmail.com>
2023-03-13 01:03:08 -0300

    :building_construction: (UI): made a index in UI folder to better import

---
+++ src/UI/index.ts
@@ -0,0 +1,25 @@
+import Breadcrumb from './Breadcrumb';
+import Button from './Button';
+import Card from './Card';
+import Select from './Select';
+import Modal from './Modal';
+import Pagination from './Pagination';
+import Table from './Table';
+import Divider from './Divider';
+import Layout from './Layout';
+import Spin from './Spin';
+import Container from './Container';
+
+export {
+  Breadcrumb,
+  Button,
+  Card,
+  Select,
+  Modal,
+  Pagination,
+  Table,
+  Divider,
+  Layout,
+  Spin,
+  Container,
+};
