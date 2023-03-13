ad64c17ff512e51a1f8e1ff7de3dd85aa95c9212
Matias Fuentes <matiasf.cortez@gmail.com>
2023-03-13 11:20:06 -0300

    :sparkles: (Added React Hook Form globally):

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import NextNProgress from 'nextjs-progressbar';
+import { Toaster } from 'react-hot-toast';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Toaster />
       <NextNProgress />
       <Component {...pageProps} />;
     </>
