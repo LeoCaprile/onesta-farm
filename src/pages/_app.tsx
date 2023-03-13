fa64c70810620949e5a3b6cc6f642d13f6ce8df1
Matias Fuentes <matiasf.cortez@gmail.com>
2023-03-13 00:05:57 -0300

    :heavy_plus_sign: (pages): Added loader to show progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css';
+import NextNProgress from 'nextjs-progressbar';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress />
+      <Component {...pageProps} />;
+    </>
+  );
 }
