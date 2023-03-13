026b1d3d8e03d2931b80de9700097653439e222b
Matias Fuentes <matiasf.cortez@gmail.com>
2023-03-13 11:19:12 -0300

    :sparkles: (UI): Added Forward Ref to Select Component

--- src/UI/Select/index.tsx
+++ src/UI/Select/index.tsx
@@ -1,43 +1,58 @@
+import { Options } from '@interfaces/select';
 import React from 'react';
 
 export interface SelectProps {
   title?: string;
   error?: boolean;
   disabled?: boolean;
-  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  name?: string;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  options?: Array<Options>;
 }
 
-const Select = ({ title, error, disabled, onChange }: SelectProps) => {
-  function hasError() {
-    if (error) {
-      return 'focus:ring-[#D04435] focus:border-[#D04435] border-[#D04435] ';
-    } else {
-      return 'focus:ring-[#034BE5] focus:border-[#034BE5] border-[#E8E8E8] ';
+const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
+  ({ title, name, error, disabled, options, onChange }, ref) => {
+    function hasError() {
+      if (error) {
+        return 'focus:ring-[#D04435] focus:border-[#D04435] border-[#D04435] ';
+      } else {
+        return 'focus:ring-[#034BE5] focus:border-[#034BE5] border-[#E8E8E8] ';
+      }
     }
-  }
-  function isDisabled() {
-    if (disabled) {
-      return 'bg-[#F5F5F5] text-[#BDBDBD]';
+    function isDisabled() {
+      if (disabled) {
+        return 'bg-[#F5F5F5] text-[#BDBDBD]';
+      }
     }
-  }
-  return (
-    <div>
-      {title && title}
-      <select
-        onChange={onChange}
-        disabled={disabled}
-        className={
-          'block w-full p-2 mb-6 text-sm border rounded-lg text-black bg-white ' +
-          hasError() +
-          isDisabled()
-        }
-      >
-        <option>Option 1</option>
-        <option>Option 2</option>
-        <option>Option 3</option>
-      </select>
-    </div>
-  );
-};
+    return (
+      <div>
+        {title && title}
+        <select
+          ref={ref}
+          name={name}
+          defaultValue=""
+          onChange={onChange}
+          disabled={disabled}
+          className={
+            'block w-full p-2 mb-6 text-sm border rounded-lg text-black bg-white ' +
+            hasError() +
+            isDisabled()
+          }
+        >
+          <option value="" disabled hidden>
+            Seleccionar
+          </option>
+          {options?.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  },
+);
+
+Select.displayName = 'Select';
 
 export default Select;
