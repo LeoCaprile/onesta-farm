819d71ab4c95d2e718d8811da3b2a3b128193d5a
Matias Fuentes <matiasf.cortez@gmail.com>
2023-03-13 00:09:23 -0300

    :lipstick: (UI): Added vertical scroll on content in Modal

--- src/UI/Modal/index.tsx
+++ src/UI/Modal/index.tsx
@@ -12,9 +12,9 @@ const Modal = ({ title, children, footer, open, setOpenModal }: ModalProps) => {
   return (
     <dialog
       open={open}
-      className="fixed top-0 left-0 right-0 z-50 w-full p-4 h-[calc(100%)] md:h-full overflow-x-auto backdrop-brightness-50 md:inset-0 overflow-y-auto bg-transparent"
+      className="fixed top-0 left-0 right-0 z-50 w-full p-4 h-[calc(100%)] md:h-full overflow-x-hidden backdrop-brightness-50 md:inset-0 overflow-y-hidden bg-transparent"
     >
-      <div className="relative mx-auto w-full h-full max-w-2xl md:h-auto">
+      <div className="relative  mx-auto w-full max-w-2xl md:h-auto">
         <div className="relative bg-white rounded-lg shadow ">
           <div className="flex items-start justify-between p-4 border-b rounded-t">
             <h3 className="text-xl font-semibold text-gray-900">{title}</h3>
@@ -40,7 +40,9 @@ const Modal = ({ title, children, footer, open, setOpenModal }: ModalProps) => {
               <span className="sr-only">Close modal</span>
             </button>
           </div>
-          <div className="p-6 space-y-6">{children}</div>
+          <div className="p-6 h-[500px] overflow-y-scroll space-y-6">
+            {children}
+          </div>
           {footer && (
             <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b ">
               {footer}
